951e2398be928f6860f4d9db49209f6d934cfcd5
arupg1896 <arupg1896@gmail.com>
2021-01-03 17:00:00 +0530

    add eslint for styling

--- src/app.js
+++ src/app.js
@@ -1,24 +1,22 @@
-const express = require('express');
-                        const config = require('./config');
-const Logger = require('./loaders/logger');
+const express = require('express')
+const config = require('./config')
+const Logger = require('./loaders/logger')
 
-                                    const app = express();
+const app = express()
 
-
-app.get('/', (req, res)=>{
-                                res.send('Hello');
+app.get('/', (req, res) => {
+  res.send('Hello')
 })
 
-
 app.listen(config.port, err => {
-    if (err) {
-        Logger.error(err);
-        process.exit(1);
-    } else {
-        Logger.info(`
+  if (err) {
+    Logger.error(err)
+    process.exit(1)
+  } else {
+    Logger.info(`
             ################################################
             🛡️  Server listening on port: ${config.port} 🛡️ 
             ################################################
-        `);
-    }
+        `)
+  }
 })
--- src/config/index.js
+++ src/config/index.js
@@ -1,25 +1,25 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 // Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const envFound = dotenv.config();
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const envFound = dotenv.config()
 
 if (envFound.error) {
-    // This error should crash whole process
-  
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️")
 }
 
 module.exports = {
 /**
    * Your favorite port
 **/
-port: parseInt(process.env.PORT, 10),
+  port: parseInt(process.env.PORT, 10),
 
- /**
+  /**
  * Used by winston logger
 **/
-logs: {
-    level: process.env.LOG_LEVEL || 'silly',
- },
+  logs: {
+    level: process.env.LOG_LEVEL || 'silly'
+  }
 
 }
